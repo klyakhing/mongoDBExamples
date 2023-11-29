d3177cbb83b3da5baa7f5987ef9958c3b08ea3fe
George Klyakhin <klyakhin@yahoo.com>
2023-11-29 10:16:11 -0600

    init commit

---
+++ src/test/java/com/example/mongodbexamples/MongodbexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodbexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbexamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
